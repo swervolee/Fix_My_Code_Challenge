6e451ba49d8179be12ffe895193868310b15b77a
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-24 21:49:56 +0300

    .

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,44 +1,24 @@
 #!/usr/bin/python3
-"""
-SQUARE CLASS
-"""
-
 
 class square():
-    """
-        Defines a class square
-    """
-
+    
     width = 0
     height = 0
 
+    
     def __init__(self, *args, **kwargs):
-        """
-        variable innititalization
-        """
-
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-
         return self.width * self.width
 
     def PermiterOfMySquare(self):
-        """
-        finds the perimeter of the square
-        """
-
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """
-        string representation of the class
-        """
-
         return "{}/{}".format(self.width, self.height)
-
 
 if __name__ == "__main__":
 
